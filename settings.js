5ce94e76070bf81792be8d141935207de23d2c6d
PCMASTER01 <goodwillemmanuel7@gmail.com>
2025-03-26 08:06:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'TechTitan', // Your name
+  botOwner: 'PcMaster', // Your name
   ownerNumber: '08036359839', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
