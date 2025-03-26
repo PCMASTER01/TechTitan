9cc97633240bbe218574ceb7d9dbb9a20ea7e464
PCMASTER01 <goodwillemmanuel7@gmail.com>
2025-03-26 12:23:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'PcMaster', // Your name
-  ownerNumber: '08036359839', //Your number
+  ownerNumber: '+2348036359839', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
